034e1da7a011d3283b36bbbaef75036231a92329
Victor Frangov <victor.frangov@gmail.com>
2023-12-19 18:31:13 -0500

    style changes

--- backend.py
+++ backend.py
@@ -20,4 +20,4 @@ def parseStore():
                     )
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- static/styles/script.js
+++ static/styles/script.js
@@ -1,7 +1,22 @@
 'use strict';
 
 const log_in = document.querySelector(".log-in");
+const close_button = document.querySelector(".close-button");
+const body = document.querySelector("body");
+const section = document.querySelector("section");
+const footer = document.querySelector("footer");
 
 log_in.addEventListener("click", () => {
-    alert("You need to log in to access this page!");
+    document.querySelector(".login_box").style.display = "block";
+    body.style.backgroundColor = "#181826";
+    section.style.backgroundColor = "#181826";
+    footer.style.backgroundColor = "#181826";
 });
+
+close_button.addEventListener("click", () => {
+    document.querySelector(".login_box").style.display = "none";
+    body.style.backgroundColor = "#31304D";
+    section.style.backgroundColor = "#B6BBC4";
+    footer.style.backgroundColor = "#F0ECE5";
+});
+
