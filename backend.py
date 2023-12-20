17ede0fa1baefa8f2e7062ce6d0f854493dd346b
Victor Frangov <victor.frangov@gmail.com>
2023-12-20 17:05:32 -0500

    auth v1

--- backend.py
+++ backend.py
@@ -1,13 +1,15 @@
 from flask import Flask, redirect, url_for, request, render_template
 from valorantstore import ValorantStore
 
-valorant_store = ValorantStore(username="jjerf", password="jaimeslespenis69", region="na")
-
 app = Flask(__name__)
 
-@app.route('/')
-def parseStore():
-    return render_template("index.html",
+@app.route('/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['username']
+        pswd = request.form['password']
+        valorant_store = ValorantStore(username=user, password=pswd, region="na")
+        return render_template("index.html",
                         val_credits=valorant_store.wallet(True)["valorant_points"],
                         rad_points=valorant_store.wallet(True)["radianite_points"],
                         kingdom_credits=valorant_store.wallet(True)["kingdom_credits"],
@@ -18,6 +20,11 @@ def parseStore():
                         dailyOffer3=valorant_store.store(True)["daily_offers"]["data"][3]["image"],      
                         logo="https://www.svgrepo.com/show/424912/valorant-logo-play-2.svg",
                     )
-
+    else:
+        return render_template("index.html",
+                               logo="https://www.svgrepo.com/show/424912/valorant-logo-play-2.svg",
+                               )
+    
+# make log in change to logged in as: username
 if __name__ == '__main__':
     app.run(debug=True)
--- static/styles/script.js
+++ static/styles/script.js
@@ -5,12 +5,17 @@ const close_button = document.querySelector(".close-button");
 const body = document.querySelector("body");
 const section = document.querySelector("section");
 const footer = document.querySelector("footer");
+const bundleOfferImage = document.querySelector("#bundleOffer > figure > img");
 
 log_in.addEventListener("click", () => {
     document.querySelector(".login_box").style.display = "block";
     body.style.backgroundColor = "#181826";
     section.style.backgroundColor = "#181826";
     footer.style.backgroundColor = "#181826";
+    document.querySelectorAll("div.grid > article > figure > img").forEach(img => {
+        img.style.opacity = "0.4";
+    });
+    bundleOfferImage.style.opacity = "0.4";
 });
 
 close_button.addEventListener("click", () => {
@@ -18,5 +23,8 @@ close_button.addEventListener("click", () => {
     body.style.backgroundColor = "#31304D";
     section.style.backgroundColor = "#B6BBC4";
     footer.style.backgroundColor = "#F0ECE5";
+    document.querySelectorAll("div.grid > article > figure > img").forEach(img => {
+        img.style.opacity = "1";
+    });
+    bundleOfferImage.style.opacity = "1";
 });
-
